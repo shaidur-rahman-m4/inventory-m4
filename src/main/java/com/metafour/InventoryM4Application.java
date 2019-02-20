715097f1621ada32006cf1028821f5704608d67f
zubayer <zubayer@zubayer-M4>
2019-02-20 11:23:55 +0600

    1st commit

---
+++ src/main/java/com/metafour/InventoryM4Application.java
@@ -0,0 +1,13 @@
+package com.metafour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryM4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryM4Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/metafour/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.metafour;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InventoryM4Application.class);
+	}
+
+}
